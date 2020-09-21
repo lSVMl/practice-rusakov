a5b8cac171350e03930992671e2bc303b60b6bb6
lSVMl <70856654+lSVMl@users.noreply.github.com>
2020-09-22 00:11:08 +0300

    Add files via upload

---
+++ practice 1.1/1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Create_Struct
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string _firstName, string _lastName, int _age)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            age = _age;
+        }
+        public override string ToString()
+        {
+            return "Имя: " + firstName + " Фамилия: " + lastName + " Возраст: " + age;
+        }
+        static void Main()
+        {
+            Person p = new Person("Tony", "Allen", 32);
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ practice 2.2/SortString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace practice_2._2__sort_string_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] sa = s.Split(" "); // чтобы разделить строку слова и поместить их в массив
+
+            Array.Sort(sa); // метод для сортировки массива слов
+
+            s = string.Join(" ", sa);
+            Console.WriteLine(s);
+        }
+    }
+}
